4f54de5471f9ce0e47532fb9aab0525d8d874cc2
tom56561 <tom56561@gmail.com>
2023-01-19 16:19:15 -0500

    Lab01

--- Lab01/main.c
+++ Lab01/main.c
@@ -9,15 +9,17 @@
 
 double power(double base, double n) {
     double res = base;
-    for (int i = 1; i < n; i++){
+    int i;
+    for (i = 1; i < n; i++){
         res *= base;
     }
     return res;
 }
 
 int main() {
-    for (double i = 1; i <= 10; i++) {
-        printf("power(2, %f) = %f\n" , i, power(2, i));
+    int i;
+    for (i = 1; i <= 10; i++) {
+        printf("power(2, %d) = %f\n" , i, power(2, i));
     }
     return 0;
 }
