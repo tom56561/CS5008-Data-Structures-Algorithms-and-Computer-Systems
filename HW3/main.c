e66b051a37e5649c0efe11be483fa5c9b3a53b69
tom56561 <tom56561@gmail.com>
2023-02-02 13:23:39 -0500

    week3

---
+++ HW3/main.c
@@ -0,0 +1,35 @@
+// Your Name Here
+// Date Here
+// Homework Assignment 3
+//
+// Compile this assignment with: gcc -g -Wall main.c -o main
+// Use this file to implement testing for your
+// doubly linked list implementation
+//
+// Include parts of the C Standard Library
+// These have been written by some other really
+// smart engineers.
+#include <stdio.h>  // For IO operations
+#include <stdlib.h> // for malloc/free
+
+// Our library that we have written.
+// Also, by a really smart engineer!
+#include "my_dll.h"
+// Note that we are locating this file
+// within the same directory, so we use quotations
+// and provide the path to this file which is within
+// our current directory.
+
+
+
+// ====================================================
+// ================== Program Entry ===================
+// ====================================================
+int main(){
+    dll_t * dll = create_dll();
+    
+ 	// TODO: Implement unit tests for testing 
+ 	// your doubly linked list implementation
+	    
+    return 0;
+}
---
+++ Lab03/estimator.c
@@ -0,0 +1,6 @@
+// Your Name Here
+// Date Here
+// CS5008 Lab03
+//
+// Implement your cycle count tool here.
+
