c70cf155deb0f6884442cef0d76e7fec4a5969c0
tom56561 <tom56561@gmail.com>
2023-02-28 10:30:47 -0500

    Lab07

---
+++ Lab07/factorial.c
@@ -0,0 +1,29 @@
+// Your Name Here
+// Date
+//
+// gcc -Wall factorial.c -o factorial
+// ./factorial
+
+#include <stdio.h>
+
+int factorial(int n){
+  // TODO: Implement iterative solution here
+
+  return 0;
+};
+
+int factorial_rec(int n){
+  // TODO: Implement recursive solution here
+
+  return 0;
+}
+
+int main(){
+
+  // Both of these should print the same result!
+  printf("factorial(10) = %d\n",factorial(10));
+  printf("factorial_rec(10) = %d\n",factorial_rec(10));
+
+
+  return 0;
+}
