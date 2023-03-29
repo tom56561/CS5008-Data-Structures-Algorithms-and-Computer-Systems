b45547e4d94fdacc7e1de59a657a6e775aec2c9a
tom56561 <tom56561@gmail.com>
2023-03-29 11:05:06 -0400

    Lab08

--- Lab08/adjacencymatrix.c
+++ Lab08/adjacencymatrix.c
@@ -131,28 +131,28 @@ int main(){
     int g_testgraph[ROWS][COLUMNS];
 
     // Generate a random graph
-    generateGraph(&g_testgraph);
+    generateGraph(&g_testgraph[0][0]);
 
     // Print out the graph
-    printGraph(&g_testgraph);
+    printGraph(&g_testgraph[0][0]);
 
     // Print out the nodeInDegree of each of the five nodes
     int i=0;
     for(i =0; i < COLUMNS; ++i){
-        printf("node %d in-degree= %d\n",i,nodeInDegree(&g_testgraph,i));
+        printf("node %d in-degree= %d\n",i,nodeInDegree(&g_testgraph[0][0],i));
     }
 
     // Print out the nodeOutDegree of each of the five nodes
     i=0;
     for(i =0; i < COLUMNS; ++i){
-        printf("node %d out-degree= %d\n",i,nodeOutDegree(&g_testgraph,i));
+        printf("node %d out-degree= %d\n",i,nodeOutDegree(&g_testgraph[0][0],i));
     }
 
     // Check which nodes '0' is connected to
     printf("Node 0 connections:");
     int j;
     for(j =0; j < COLUMNS; ++j){
-        printf("%d ",isConnected(&g_testgraph,0,j));
+        printf("%d ",isConnected(&g_testgraph[0][0],0,j));
     }
 
     return 0;
